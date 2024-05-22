0f84d6b67e76a5e04e5c058cff2b2978b1fb1678
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-05-22 23:11:27 +0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,36 +1,45 @@
 mod custom_errors;
+mod ftp;
+mod http;
+mod ports;
+
+use crate::http::{GetServerVersion, HttpFeature};
+use crate::ftp::{FTPFeature, FTPAuthorization};
 use std::sync::Arc;
 use tokio::time::timeout;
 use tokio::net::TcpStream;
 use custom_errors::Errors;
 use std::io;
 use std::net::IpAddr;
-use reqwest::{Client, Proxy};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::sync::Semaphore;
 use tokio::task::JoinHandle;
 use tokio::time::Duration;
-
-
+use ports::{Ports, ports};
+use env_logger;
+use log::{info, error, warn};
+const DEFAULT_MAX_PARALLEL_TCP_CONNECTIONS: usize = 1000;
+const TIME_OUT_PROGRAMS: u64 = 3;
 
 #[tokio::main]
 async fn main() -> Result<(), Errors>{
-    let semaphore = Arc::new(Semaphore::new(1000));
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
 
-    println!("Впишите чистое IP");
+    let parallel_tcp_connection_limiter = Arc::new(Semaphore::new(DEFAULT_MAX_PARALLEL_TCP_CONNECTIONS));
+    info!("Впишите чистое IP");
     let input_user = ip_input_user()?;
-    println!("От какого порта сканирование");
+    info!("От какого порта сканирование");
     let first_input_user = int_input_user()?;
-    println!("До какого порта сканирование");
+    info!("До какого порта сканирование");
     let second_input_user = int_input_user()?;
 
     let async_thread: JoinHandle<Result<(), Errors>> =  tokio::spawn(async move{
-        scan_ports(&input_user, first_input_user, second_input_user, semaphore).await?;
+        scan_ports(&input_user, first_input_user, second_input_user, parallel_tcp_connection_limiter).await?;
         Ok(())
     });
     async_thread.await??;
 
-    println!("Выполнение программы закончено!");
+    warn!("Выполнение программы закончено!");
     Ok(())
 }
 
@@ -39,49 +48,50 @@ async fn port_scan(target: &str, port: u16) {
     let timeout_duration = Duration::from_secs(5);
     match timeout(timeout_duration, TcpStream::connect((target, port))).await{
         Ok(Ok(_)) => match port_type {
-            Ports::HTTPS => println!("HTTPS - {}:{}", target, port),
-            Ports::HTTP => println!("HTTP - {}:{}", target, port),
-            Ports::FTP => println!("FTP - {}:{}", target, port),
-            Ports::SSH => println!("SSH - {}:{}", target, port),
-            Ports::SMTP => println!("SMTP - {}:{}", target, port),
-            Ports::POP3 => println!("POP3 - {}:{}", target, port),
-            Ports::IMAP => println!("IMAP - {}:{}", target, port),
-            Ports::DNS => println!("DNS - {}:{}", target, port),
-            Ports::MYSQL => println!("MYSQL - {}:{}", target, port),
-            Ports::DHCP => println!("DHCP - {}:{}", target, port),
-            Ports::RDP => println!("RDP - {}:{}", target, port),
-            Ports::Telnet => println!("Telnet - {}:{}", target, port),
-            Ports::Redis => println!("Redis - {}:{}", target, port),
-            Ports::POP3S => println!("POP3S - {}:{}", target, port),
-            Ports::LDAP => println!("LDAP - {}:{}", target, port),
-            Ports::SNMP => println!("SNMP - {}:{}", target, port),
-            Ports::SMTPS => println!("SMTPS - {}:{}", target, port),
-            Ports::IMAPS => println!("IMAPS - {}:{}", target, port),
-            Ports::PostgreSQL => println!("PostgreSQL - {}:{}", target, port),
-            Ports::CPanel => println!("cPanel - {}:{}", target, port),
-            Ports::HttpProxy => println!("HttpProxy - {}:{}", target, port),
-            Ports::HostingHttp => println!("HostingHttp - {}:{}", target, port),
-            Ports::WHM => println!("WHM - {}:{}", target, port),
-            Ports::WhmSsL => println!("WHM_SSL - {}:{}", target, port),
-            Ports::Other => println!("Other - {}:{}", target, port),
-
+            Ports::HTTPS => info!("HTTPS - {}:{}", target, port),
+            Ports::HTTP => info!("HTTP - {}:{}", target, port),
+            Ports::FTP => info!("FTP - {}:{}", target, port),
+            Ports::SSH => info!("SSH - {}:{}", target, port),
+            Ports::SMTP => info!("SMTP - {}:{}", target, port),
+            Ports::POP3 => info!("POP3 - {}:{}", target, port),
+            Ports::IMAP =>  info!("IMAP - {}:{}", target, port),
+            Ports::DNS => info!("DNS - {}:{}", target, port),
+            Ports::MYSQL => info!("MYSQL - {}:{}", target, port),
+            Ports::DHCP => info!("DHCP - {}:{}", target, port),
+            Ports::RDP => info!("RDP - {}:{}", target, port),
+            Ports::Telnet => info!("Telnet - {}:{}", target, port),
+            Ports::Redis => info!("Redis - {}:{}", target, port),
+            Ports::POP3S => info!("POP3S - {}:{}", target, port),
+            Ports::LDAP => info!("LDAP - {}:{}", target, port),
+            Ports::SNMP => info!("SNMP - {}:{}", target, port),
+            Ports::SMTPS => info!("SMTPS - {}:{}", target, port),
+            Ports::IMAPS => info!("IMAPS - {}:{}", target, port),
+            Ports::PostgreSQL => info!("PostgreSQL - {}:{}", target, port),
+            Ports::CPanel => info!("cPanel - {}:{}", target, port),
+            Ports::HttpProxy => info!("HttpProxy - {}:{}", target, port),
+            Ports::HostingHttp => info!("HostingHttp - {}:{}", target, port),
+            Ports::WHM => info!("WHM - {}:{}", target, port),
+            Ports::WhmSsL => info!("WHM_SSL - {}:{}", target, port),
+            Ports::Other => info!("Other - {}:{}", target, port),
         },
-        Ok(Err(e)) => println!("Ошибка подключения к порту - {}", e),
+        Ok(Err(e)) => warn!("Ошибка подключения к порту - {}", e),
         Err(_) => (),
     }
 }
 
-async fn scan_ports(target: &str, start_port: u16, end_port: u16, semaphore: Arc<Semaphore>) -> Result<(), Errors>{
+async fn scan_ports(target: &str, start_port: u16, end_port: u16, parallel_tcp_connection_limiter: Arc<Semaphore>) -> Result<(), Errors>{
     let mut list = Vec::new();
 
     for port in start_port..=end_port{
         let buffer = target.to_string();
-        let cloned_semaphore = semaphore.clone();
+        let cloned_parallel_tcp_connection_limiter = parallel_tcp_connection_limiter.clone();
+
         let handle: JoinHandle<Result<(), Errors>> = tokio::spawn( async move  {
-            let permit = cloned_semaphore.acquire().await.map_err(|e|{
+            let permit = cloned_parallel_tcp_connection_limiter.acquire().await.map_err(|e|{
                 eprintln!("Ошибка получения разрешения симафора! - {}", e);
                 Errors::Error
             })?;
+
             port_scan(&buffer, port).await;
             drop(permit);
             Ok(())
@@ -92,6 +102,7 @@ async fn scan_ports(target: &str, start_port: u16, end_port: u16, semaphore: Arc
     for handle in list{
         handle.await??;
     }
+
     check_servers(target).await?;
     Ok(())
 }
@@ -100,11 +111,12 @@ fn ip_input_user() -> Result<String, Errors>{
     let mut buffer = String::new();
 
     io::stdin().read_line(&mut buffer).map_err(|_| {
-        eprintln!("Ошибка пользовательствого ввода");
+        error!("Ошибка пользовательствого ввода");
         Errors::Error
     })?;
+
     buffer.trim().parse::<IpAddr>().map_err(|e|{
-        eprintln!("Введен неправильный Ip-Address - {}", e);
+        error!("Введен неправильный Ip-Address - {}", e);
         Errors::Error
     })?;
 
@@ -115,187 +127,44 @@ fn int_input_user() -> Result<u16, Errors>{
     let mut buffer = String::new();
 
     io::stdin().read_line(&mut buffer).map_err(|_| {
-        eprintln!("Ошибка пользовательствого ввода");
+        error!("Ошибка пользовательствого ввода");
         Errors::Error
     })?;
+
     buffer.trim().parse::<u16>().map_err(|e| {
-        eprintln!("Ошибка парсинга - {}", e);
+        error!("Ошибка парсинга - {}", e);
         Errors::Error
     })
 }
-
-async fn get_version(target: &str) -> Result<(), Errors>{
-    let port_list = [80, 443, 8080, 8443, 8880];
-    let proxy = create_proxy()?;
-    let client = create_client(proxy)?;
-
-    for port in &port_list{
-        let url = format!("http://{}:{}", target, port);
-        match client.get(&url).send().await{
-            Ok(response) => {
-                if let Some(server_header) = response.headers().get("Server"){
-                    let version = server_header.to_str().unwrap();
-                    println!("-----------------------------------------------");
-                    println!("Версия сервера порта: {} - {}", port, version);
-                    println!("-----------------------------------------------");
-                    println!(" ");
-                }
-                else{
-                    eprintln!("Ошибка получения хэдера сервера! Статус - {}", response.status().to_string())
-                }
-            }
-            Err(e) => {
-                eprintln!("Ошибка получения ответа - {}", e)
-            },
-        };
-    }
-
-    Ok(())
-}
-
-async fn check_auth_ftp(target: &str) -> Result<(), Errors> {
-    let url = format!("{}:21", target);
-    let timeout_dur = Duration::from_secs(5);
-
-    let mut stream = match timeout(timeout_dur, TcpStream::connect(&url)).await {
-        Ok(Ok(stream)) => stream,
-        Err(e) => {
-            eprintln!("Таймаут истек: {}", e);
-            return Err(Errors::Error);
-        },
-        Ok(Err(e)) => {
-            eprintln!("Неопознанная ошибка - {}", e);
-            return Err(Errors::Error);
-        }
-    };
-
-    let request = b"USER anonymous\r\n";
-    stream.write_all(request).await.map_err(|e|{
-        eprintln!("Ошибка записи данных в поток! - {}", e);
-        Errors::Error
-    })?;
-
-    let mut buffer = [0; 1024];
-    let n = stream.read(&mut buffer).await.map_err(|e|{
-        eprintln!("Ошибка записи данных из потока! - {}", e);
-        Errors::Error
-    })?;
-
-    let response = &buffer[..n];
-    println!("Response: {}", String::from_utf8_lossy(response));
-
-    Ok(())
-}
-
-fn ports(port: &u16) -> Ports{
-    match port{
-        20 => Ports::FTP,
-        21 => Ports::FTP,
-        22 => Ports::SSH,
-        23 => Ports::Telnet,
-        25 => Ports::SMTP,
-        53 => Ports::DNS,
-        67 => Ports::DHCP,
-        68 => Ports::DHCP,
-        110 => Ports::POP3,
-        143 => Ports::IMAP,
-        161 => Ports::SNMP,
-        162 => Ports::SNMP,
-        389 => Ports::LDAP,
-        443 => Ports::HTTPS,
-        465 => Ports::SMTPS,
-        587 => Ports::SMTP,
-        993 => Ports::IMAPS,
-        995 => Ports::POP3S,
-        2083 => Ports::CPanel,
-        2086 => Ports::WHM,
-        2087 => Ports::WhmSsL,
-        3306 => Ports::MYSQL,
-        3389 => Ports::RDP,
-        5432 => Ports::PostgreSQL,
-        6379 => Ports::Redis,
-        8080 => Ports::HttpProxy,
-        8880 => Ports::HTTP,
-        8443 => Ports::HostingHttp,
-        80 => Ports::HTTP,
-        _ => Ports::Other,
-    }
-}
-
-fn create_client(proxy: Proxy) -> Result<Client, Errors>{
-    Client::builder()
-        .timeout(Duration::from_secs(5))
-        .proxy(proxy)
-        .build()
-        .map_err(|e|{
-            eprintln!("Ошибка создания клиента! - {}", e);
-            Errors::Error
-        })
-}
-
-fn create_proxy() -> Result<Proxy, Errors>{
-    Proxy::https("116.203.207.197:8080").map_err(|e|{
-        eprintln!("Ошибка создания прокси! - {}", e);
-        Errors::Error
-    })
-}
-
-fn input_user() -> Result<String, Errors>{
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).map_err(|e| {
-        eprintln!("Ошибка ввода пользователя - {}", e);
-        Errors::Error
-    })?;
-    Ok(buffer.trim().to_string())
-}
-
 async fn check_servers(target: &str) -> Result<(), Errors>{
-    println!("Хотите получить версию сервера?  Y/N");
-
-    if input_user()?.to_lowercase() == "y"{
-        println!("Выберите: \n1.Http-Https\n2.FTP");
-        loop{
-            let user_input = int_input_user()?;
-            match user_input{
-                1 => {
-                    get_version(target).await?;
-                    println!("Выберите: \n1.HTTP-HTTPS\n2.FTP");
-                },
-                2 => {
-                    if let Err(_) = check_auth_ftp(target).await{}
-                    println!("Выберите: \n1.Http-Https\n2.FTP");
+    loop{
+        info!("1.Get => Http-Headers");
+        info!("2.Get => Ftp Authorization");
+        info!("Other input => leave");
+        let user_input = int_input_user()?;
+        match user_input{
+            1 => {
+                let features = Ports::get_version(target).await.map_err(|_|{
+                    Errors::Error
+                })?;
+                for feature in &features{
+                    match feature {
+                        HttpFeature::ServerVersion(version) => info!("{}",version),
+                    }
                 }
-                _ => break
             }
+            2 => {
+                let features = Ports::ftp_authorization(target).await.map_err(|_|{
+                        Errors::Error
+                    })?;
+                for feature in &features{
+                    match feature{
+                        FTPFeature::Anon(auth) => info!("{}", auth),
+                    }
+                }
+            }
+            _ => break
         }
     }
     Ok(())
-}
-
-enum Ports{
-    FTP,
-    SSH,
-    Telnet,
-    SMTP,
-    POP3,
-    IMAP,
-    HTTPS,
-    HTTP,
-    DNS,
-    DHCP,
-    SNMP,
-    LDAP,
-    SMTPS,
-    IMAPS,
-    POP3S,
-    MYSQL,
-    RDP,
-    PostgreSQL,
-    Redis,
-    Other,
-    CPanel,
-    HttpProxy,
-    HostingHttp,
-    WHM,
-    WhmSsL,
 }
