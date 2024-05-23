651f86308ddeed0ff56ae751624d0db90b7c3b96
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-05-23 19:01:19 +0300

    Update ssh.rs

--- ssh.rs
+++ ssh.rs
@@ -1,3 +1,4 @@
+use crate::warn;
 use std::time::Duration;
 use log::{error};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -12,38 +13,30 @@ pub async fn ssh_version(target: &str) -> Result<Vec<Features>, Errors> {
     let url = format!("{}:{}",target, &ssh_port);
     let timeout_duration = Duration::from_secs(TIME_OUT_PROGRAMS);
 
-    let mut tcp_stream = match timeout (timeout_duration, TcpStream::connect(&url)).await{
-        Ok(Ok(stream)) => stream,
-        Err(e) => {
-            error!("Ошибка подключения к порту - {}", e);
-            return Err(Errors::Error);
-        }
-        Ok(Err(e)) => {
-            error!("Ошибка пермишена - {}", e);
-            return Err(Errors::Error)
-        },
-    };
+   if let Ok(Ok(mut tcp_stream)) = timeout (timeout_duration, TcpStream::connect(&url)).await{
+       let request = b"SSH-2.0\n";
+       tcp_stream.write_all(request).await.map_err(|e|{
+           error!("Ошибка отправки! - {}", e);
+           Errors::Error
+       })?;
 
-    let request = b"SSH-2.0\n";
-    tcp_stream.write_all(request).await.map_err(|e|{
-        error!("Ошибка отправки! - {}", e);
-        Errors::Error
-    })?;
+       let mut buffer = [0; 1024];
+       tcp_stream.read(&mut buffer).await.map_err(|e|{
+           error!("Ошибка записи! - {}", e);
+           Errors::Error
+       })?;
 
-    let mut buffer = [0; 1024];
-    tcp_stream.read(&mut buffer).await.map_err(|e|{
-        error!("Ошибка записи! - {}", e);
-        Errors::Error
-    })?;
-
-    if buffer.is_empty(){
-        error!("Пустой буффер!");
-        return Err(Errors::Error)
-    };
-
-    let version_brute = String::from_utf8_lossy(&mut buffer);
-    if let Some(version) = version_brute.lines().next(){
-        features_list.push(Features::SSHVersion(version.to_string()));
-    }
+       if buffer.is_empty(){
+           error!("Пустой буффер!");
+           return Err(Errors::Error)
+       };
+       let version_brute = String::from_utf8_lossy(&mut buffer);
+       if let Some(version) = version_brute.lines().next(){
+           features_list.push(Features::SSHVersion(version.to_string()));
+       }
+   }
+   else{
+       warn!("Таймаут - ошибка подключения к порту!");
+   }
     Ok(features_list)
 }
