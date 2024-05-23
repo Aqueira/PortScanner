e54b8d468b2453686cfcd527390cc402ccbee636
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-05-23 18:15:31 +0300

    Update http.rs

--- http.rs
+++ http.rs
@@ -2,39 +2,32 @@ use std::time::Duration;
 use log::error;
 use reqwest::{Client, Proxy};
 use crate::custom_errors::Errors;
-use crate::ports::Ports;
-use crate::TIME_OUT_PROGRAMS;
+use crate::{Features, TIME_OUT_PROGRAMS};
 
-pub enum HttpFeature{
-    ServerVersion(String)
-}
-pub trait GetServerVersion{
-    async fn get_version(target: &str) -> Result<Vec<HttpFeature>, Errors>;
-}
-impl GetServerVersion for Ports{
-    async fn get_version(target: &str) -> Result<Vec<HttpFeature>, Errors> {
-        let port_list = [80, 443, 8080, 8443, 8880];
-        let client = create_client()?;
-        let mut features_list = vec![];
 
-        for port in &port_list {
-            let url = format!("http://{}:{}", target, port);
-            match client.get(&url).send().await {
-                Ok(response) => {
-                    if let Some(server_header) = response.headers().get("Server") {
-                        let version = format!("{}:{} -> {:?}",target, port, server_header);
-                        features_list.push(HttpFeature::ServerVersion(version));
-                    }
-                    else {
-                        error!("Ошибка получения хэдера сервера! Статус - {}", response.status().to_string())
-                    }
+pub async fn get_version(target: &str) -> Result<Vec<Features>, Errors> {
+    let port_list = [80, 443, 8080, 8443, 8880];
+    let client = create_client()?;
+    let mut features_list = vec![];
+
+    for port in &port_list {
+        let url = format!("http://{}:{}", target, port);
+        match client.get(&url).send().await {
+            Ok(response) => {
+                if let Some(server_header) = response.headers().get("Server") {
+                    let version = format!("{}:{} -> {:?}",target, port, server_header);
+                    features_list.push(Features::HttpVersion(version));
                 }
-                Err(_) => (),
-            };
-        }
-        Ok(features_list)
+                else {
+                    error!("Ошибка получения хэдера сервера! Статус - {}", response.status().to_string())
+                }
+            }
+            Err(_) => (),
+        };
     }
+    Ok(features_list)
 }
+
 fn create_client() -> Result<Client, Errors>{
     let proxy = Proxy::https("116.203.207.197:8080").map_err(|e|{
         error!("Ошибка создания прокси клиента! - {}", e);
