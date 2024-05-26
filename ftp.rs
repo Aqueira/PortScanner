c8d2fda15c593aa4b87642c6927e83800b1c5fe1
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-05-26 21:20:54 +0300

    Update ftp.rs

--- ftp.rs
+++ ftp.rs
@@ -1,12 +1,14 @@
+use std::net::IpAddr;
 use crate::{Features, TIME_OUT_PROGRAMS};
 use std::time::Duration;
-use log::{error, warn};
+use log::{warn};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use crate::custom_errors::Errors;
 use tokio::time::timeout;
+use crate::custom_errors::Errors;
 
-pub async fn ftp_authorization(target: &str) -> Result<Vec<Features>, Errors>{
+
+pub async fn ftp_authorization(target: &IpAddr) -> Result<Vec<Features>, Errors>{
     let timeout_tcp_duration = Duration::from_secs(TIME_OUT_PROGRAMS);
     let ftp_ports =  vec![20,21];
     let mut features = vec![];
@@ -16,33 +18,36 @@ pub async fn ftp_authorization(target: &str) -> Result<Vec<Features>, Errors>{
         let mut stream = match timeout(timeout_tcp_duration, TcpStream::connect(&url)).await{
             Ok(Ok(stream)) => stream,
             Err(e) => {
-                warn!("Подключение до порта {} не установлено! - {}", port, e);
+                warn!("Таймаут подключения к порту - {} не установлено! - {}", port, e);
                 continue
             },
-            Ok(Err(_)) => continue,
+            Ok(Err(e)) => {
+                warn!("Ошибка подключения к порту - {} не установлено! - {}", port,  e);
+                continue
+            },
         };
 
         let request = b"USER anonymous\r\n";
-        stream.write_all(request).await.map_err(|e|{
-            error!("Ошибка записи данных в поток! - {}", e);
-            Errors::Error
-        })?;
+        match stream.write_all(request).await{
+            Ok(_) => (),
+            Err(e) => warn!("Ошибка отправки запроса! - {}", e)
+        }
 
         let mut buffer = [0; 1024];
-        let read = stream.read(&mut buffer).await.map_err(|e|{
-            error!("Ошибка чтения данных из потока! - {}", e);
-            Errors::Error
-        })?;
+        match stream.read(&mut buffer).await{
+            Ok(read) => {
+                let response = &buffer[0..read];
+                let buffered_text = String::from_utf8_lossy(response);
+                let target_line = "No anonymous login";
 
-        let response = &buffer[0..read];
-        let buffered_text = String::from_utf8_lossy(response);
-        let target_line = "No anonymous login";
-
-        if buffered_text.contains(target_line){
-            features.push(Features::FTPAuthorization(format!("FTP connect => Rejected - {}:{}",target, port)));
-        }
-        else{
-            features.push(Features::FTPAuthorization(format!("FTP connect => Accepted - {}:{}",target, port)));
+                if buffered_text.contains(target_line){
+                    features.push(Features::FTPAuthorization(format!("FTP connect => Rejected - {}:{}",target, port)));
+                }
+                else{
+                    features.push(Features::FTPAuthorization(format!("FTP connect => Accepted - {}:{}",target, port)));
+                }
+            }
+            Err(e) => warn!("Ошибка чтения! - {}", e)
         }
     }
     Ok(features)
