90cf437e3864adabc6285075926fbd855d470088
ReDestroyDeR <daniil.schroder@gmail.com>
2024-05-29 01:04:42 +0300

    я твою хуйню тут поправил

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use crate::ftp::ftp_features;
 use crate::http::http_features;
 use crate::ssh::ssh_features;
+use aqueiralibrary::{input, ParseInput};
 use custom_errors::Error;
+use env_logger;
+use log::{info, warn};
+use ports::Port;
+use std::any::Any;
+use std::net::IpAddr;
+use std::sync::Arc;
+use tokio::net::TcpStream;
+use tokio::sync::Mutex;
+use tokio::sync::Semaphore;
+use tokio::task::JoinHandle;
+use tokio::time::timeout;
+use tokio::time::Duration;
 
-#[derive(PartialEq)]
-#[derive(Debug)]
-pub enum Features{
+mod custom_errors;
+mod ftp;
+mod http;
+mod ports;
+mod ssh;
+
+#[derive(PartialEq, Debug)]
+pub enum Features {
     //#HTTP
     HttpVersion(String),
     //...
@@ -26,74 +44,70 @@ async fn main() -> Result<(), Error> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let parallel_tcp_connection_limiter = Arc::new(Semaphore::new(DEFAULT_MAX_PARALLEL_TCP_CONNECTIONS));
-    info!("Впишите чистое IP");
+    let parallel_tcp_connection_limiter =
+        Arc::new(Semaphore::new(DEFAULT_MAX_PARALLEL_TCP_CONNECTIONS));
+    info!("Впишите IPv4");
     let input_user: IpAddr = input()?.parsing()?;
     info!("От какого порта сканирование");
     let first_input_user: u16 = input()?.parsing()?;
     info!("До какого порта сканирование");
     let second_input_user: u16 = input()?.parsing()?;
 
-    let async_thread = tokio::spawn(async move {
-        scan_ports(input_user, first_input_user, second_input_user, parallel_tcp_connection_limiter).await?
+    let async_thread: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
+        scan_ports(
+            input_user,
+            first_input_user,
+            second_input_user,
+            parallel_tcp_connection_limiter,
+        )
+        .await
     });
     async_thread.await??;
 
     let async_thread: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
-        user_interface(get_features(&input_user).await.map_err(|e| Error::any("Ошибка получения особенностей!", e))?).await?;
-        Ok(())
+        user_interface(
+            get_features(&input_user)
+                .await
+                .map_err(|e| Error::any("Ошибка получения особенностей!", e))?,
+        )
+        .await
     });
     async_thread.await??;
 
     warn!("Выполнение программы закончено!");
     Ok(())
-
 }
 
-async fn port_scan(target: IpAddr, port: u16){
-    let port_type = ports(&port);
+async fn port_scan(target: IpAddr, port: Port) {
     let timeout_duration = Duration::from_secs(5);
 
-    match timeout(timeout_duration, TcpStream::connect((target, port))).await{
-        Ok(Ok(_)) => match port_type {
-            Ports::HTTPS => info!("HTTPS - {}:{}", target, port),
-            Ports::HTTP => info!("HTTP - {}:{}", target, port),
-            Ports::FTP => info!("FTP - {}:{}", target, port),
-            Ports::SSH => info!("SSH - {}:{}", target, port),
-            Ports::SMTP => info!("SMTP - {}:{}", target, port),
-            Ports::POP3 => info!("POP3 - {}:{}", target, port),
-            Ports::IMAP =>  info!("IMAP - {}:{}", target, port),
-            Ports::DNS => info!("DNS - {}:{}", target, port),
-            Ports::MYSQL => info!("MYSQL - {}:{}", target, port),
-            Ports::DHCP => info!("DHCP - {}:{}", target, port),
-            Ports::RDP => info!("RDP - {}:{}", target, port),
-            Ports::Telnet => info!("Telnet - {}:{}", target, port),
-            Ports::Redis => info!("Redis - {}:{}", target, port),
-            Ports::POP3S => info!("POP3S - {}:{}", target, port),
-            Ports::LDAP => info!("LDAP - {}:{}", target, port),
-            Ports::SNMP => info!("SNMP - {}:{}", target, port),
-            Ports::SMTPS => info!("SMTPS - {}:{}", target, port),
-            Ports::IMAPS => info!("IMAPS - {}:{}", target, port),
-            Ports::PostgreSQL => info!("PostgreSQL - {}:{}", target, port),
-            Ports::CPanel => info!("cPanel - {}:{}", target, port),
-            Ports::HttpProxy => info!("HttpProxy - {}:{}", target, port),
-            Ports::HostingHttp => info!("HostingHttp - {}:{}", target, port),
-            Ports::WHM => info!("WHM - {}:{}", target, port),
-            Ports::WhmSsL => info!("WHM_SSL - {}:{}", target, port),
-            Ports::Other => info!("Other - {}:{}", target, port),
-        },
+    match timeout(timeout_duration, TcpStream::connect((target, port.num))).await {
+        Ok(Ok(_)) => info!("{} - {}:{}", port.name, target, port.num),
         Ok(Err(e)) => warn!("Ошибка подключения к порту - {}", e),
         Err(_) => (),
     }
 }
 
-async fn scan_ports(target: IpAddr, start_port: u16, end_port: u16, parallel_tcp_connection_limiter: Arc<Semaphore>) -> Result<(), Error>{
+async fn scan_ports(
+    target: IpAddr,
+    start_port: u16,
+    end_port: u16,
+    parallel_tcp_connection_limiter: Arc<Semaphore>,
+) -> Result<(), Error> {
     let mut list = Vec::new();
 
-    for port in start_port..=end_port{
+    for port in (start_port..=end_port).map(|port| Port::from(port)) {
         let cloned_parallel_tcp_connection_limiter = parallel_tcp_connection_limiter.clone();
-        let async_thread: JoinHandle<Result<(), Error>> = tokio::spawn( async move  {
-            let permit = cloned_parallel_tcp_connection_limiter.acquire().await.map_err(|e| Error::any("Ошибка получения разрешения симафора!", e))?;
+        let async_thread: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
+            let permit = cloned_parallel_tcp_connection_limiter
+                .acquire()
+                .await
+                .map_err(|e| {
+                    Error::any(
+                        "Достигнут максимум параллельных подключений сканирования",
+                        e,
+                    )
+                })?;
             port_scan(target, port).await;
             drop(permit);
             Ok(())
@@ -101,16 +115,16 @@ async fn scan_ports(target: IpAddr, start_port: u16, end_port: u16, parallel_tcp
         list.push(async_thread);
     }
 
-    for handle in list{
+    for handle in list {
         handle.await??;
     }
 
     Ok(())
 }
 
-async fn user_interface(features: Vec<Features>) -> Result<(), Error>{
-    for feature in &features{
-        match feature{
+async fn user_interface(features: Vec<Features>) -> Result<(), Error> {
+    for feature in &features {
+        match feature {
             Features::FTPAuth(auth) => warn!("{}", auth),
             Features::HttpVersion(version) => warn!("{}", version),
             Features::SSHVersion(version) => warn!("{}", version),
@@ -121,11 +135,20 @@ async fn user_interface(features: Vec<Features>) -> Result<(), Error>{
 }
 
 async fn get_features(target: &IpAddr) -> Result<Vec<Features>, Error> {
-    let http_features = http_features(target).await.map_err(|e| Error::any("Ошибка получения HTTP особенностей!", e))?;
-    let ssh_features = ssh_features(target).await.map_err(|e| Error::any("Ошибка получения SSH особенностей!", e))?;
-    let ftp_features = ftp_features(target).await.map_err(|e| Error::any("Ошибка получения FTP особенностей!", e))?;
+    let http_features = http_features(target)
+        .await
+        .map_err(|e| Error::any("Ошибка получения HTTP особенностей!", e))?;
+    let ssh_features = ssh_features(target)
+        .await
+        .map_err(|e| Error::any("Ошибка получения SSH особенностей!", e))?;
+    let ftp_features = ftp_features(target)
+        .await
+        .map_err(|e| Error::any("Ошибка получения FTP особенностей!", e))?;
 
-    let new_vector: Vec<Features> = vec![http_features, ssh_features, ftp_features].into_iter().flatten().collect();
+    let new_vector: Vec<Features> = vec![http_features, ssh_features, ftp_features]
+        .into_iter()
+        .flatten()
+        .collect();
 
     Ok(new_vector)
 }
--- src/ports.rs
+++ src/ports.rs
@@ -1,4 +1,53 @@
-pub enum Ports{
+pub struct Port {
+    pub num: u16,
+    pub name: String,
+    pub port_type: PortType,
+}
+
+impl Port {
+    pub fn new(num: u16, name: &str, port_type: PortType) -> Self {
+        Self {
+            num,
+            name: name.to_string(),
+            port_type,
+        }
+    }
+
+    pub fn from(port: u16) -> Self {
+        match port {
+            20 => Self::new(port, "FTP", PortType::FTP),
+            21 => Self::new(port, "Secure FTP", PortType::FTP),
+            22 => Self::new(port, "SSH", PortType::SSH),
+            23 => Self::new(port, "Telnet", PortType::Telnet),
+            25 => Self::new(port, "SMTP", PortType::SMTP),
+            53 => Self::new(port, "DNS", PortType::DNS),
+            67 | 68 => Self::new(port, "DHCP", PortType::DHCP),
+            110 => Self::new(port, "POP3", PortType::POP3),
+            143 => Self::new(port, "IMAP", PortType::IMAP),
+            161 | 162 => Self::new(port, "SNMP", PortType::SNMP),
+            389 => Self::new(port, "LDAP", PortType::LDAP),
+            443 => Self::new(port, "HTTPS", PortType::HTTPS),
+            465 => Self::new(port, "SMTPS", PortType::SMTPS),
+            587 => Self::new(port, "SMTP", PortType::SMTP),
+            993 => Self::new(port, "IMAPS", PortType::IMAPS),
+            995 => Self::new(port, "POP3s", PortType::POP3S),
+            2083 => Self::new(port, "CPanel", PortType::CPanel),
+            2086 => Self::new(port, "WHM", PortType::WHM),
+            2087 => Self::new(port, "WhmSsL", PortType::WhmSsL),
+            3306 => Self::new(port, "MySQL", PortType::MYSQL),
+            3389 => Self::new(port, "RDP", PortType::RDP),
+            5432 => Self::new(port, "PostgreSQL", PortType::PostgreSQL),
+            6379 => Self::new(port, "Redis", PortType::Redis),
+            8080 => Self::new(port, "HttpProxy", PortType::HttpProxy),
+            8880 => Self::new(port, "HTTP", PortType::HTTP),
+            8443 => Self::new(port, "HostingHttp", PortType::HostingHttp),
+            80 => Self::new(port, "HTTP", PortType::HTTP),
+            _ => Self::new(port, "Other", PortType::Other),
+        }
+    }
+}
+
+pub enum PortType {
     FTP,
     SSH,
     Telnet,
@@ -18,42 +67,10 @@ pub enum Ports{
     RDP,
     PostgreSQL,
     Redis,
-    Other,
     CPanel,
     HttpProxy,
     HostingHttp,
     WHM,
     WhmSsL,
-}
-pub fn ports(port: &u16) -> Ports{
-    match port{
-        20 | 21 => Ports::FTP,
-        22 => Ports::SSH,
-        23 => Ports::Telnet,
-        25 => Ports::SMTP,
-        53 => Ports::DNS,
-        67 => Ports::DHCP,
-        68 => Ports::DHCP,
-        110 => Ports::POP3,
-        143 => Ports::IMAP,
-        161 | 162 => Ports::SNMP,
-        389 => Ports::LDAP,
-        443 => Ports::HTTPS,
-        465 => Ports::SMTPS,
-        587 => Ports::SMTP,
-        993 => Ports::IMAPS,
-        995 => Ports::POP3S,
-        2083 => Ports::CPanel,
-        2086 => Ports::WHM,
-        2087 => Ports::WhmSsL,
-        3306 => Ports::MYSQL,
-        3389 => Ports::RDP,
-        5432 => Ports::PostgreSQL,
-        6379 => Ports::Redis,
-        8080 => Ports::HttpProxy,
-        8880 => Ports::HTTP,
-        8443 => Ports::HostingHttp,
-        80 => Ports::HTTP,
-        _ => Ports::Other,
-    }
+    Other,
 }
