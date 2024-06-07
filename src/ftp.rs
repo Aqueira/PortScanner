e6c17ec862912a0148259760def2aff8b89190cf
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-06-07 13:06:07 +0300

    Update ftp.rs

--- src/ftp.rs
+++ src/ftp.rs
@@ -1,11 +1,9 @@
+use crate::traits::{ProtocolOperation, ProtocolOperations};
 use crate::Error;
-use crate::{Features, TIME_OUT_PROGRAMS};
-use log::warn;
+use crate::Features;
+use crate::Features::FTPAuth;
 use std::net::IpAddr;
-use std::time::Duration;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::time::timeout;
 
 pub async fn ftp_features(target: &IpAddr) -> Result<Vec<Features>, Error> {
     let mut ftp_features = vec![];
@@ -16,58 +14,54 @@ pub async fn ftp_features(target: &IpAddr) -> Result<Vec<Features>, Error> {
     }
     Ok(ftp_features)
 }
+
 async fn ftp_authorization(target: &IpAddr) -> Result<Vec<Features>, Error> {
-    let timeout_tcp_duration = Duration::from_secs(TIME_OUT_PROGRAMS);
     let ftp_ports = vec![20, 21];
-    let mut anon_auths = vec![];
+    let mut features = vec![];
 
     for port in &ftp_ports {
         let url = format!("{}:{}", target, port);
-        let mut stream = match timeout(timeout_tcp_duration, TcpStream::connect(&url)).await {
-            Ok(Ok(stream)) => stream,
-            Err(e) => {
-                warn!(
-                    "├─ [Error] Таймаут подключения к порту - {} не установлено! - {}",
-                    port, e
-                );
-                continue;
+        if let Some(mut stream) = establish_connection(&url, port).await {
+            send_request(&mut stream).await;
+            let mut buffer = [0u8; 1024];
+            if let Some(read) = get_response_request(&mut stream, &mut buffer).await {
+                let text = convert_text_to_string(&mut buffer, read);
+                let result = get_status_auth(&text, target, port);
+                features.push(result);
             }
-            Ok(Err(e)) => {
-                warn!(
-                    "├─ [CriticalError] Ошибка подключения к порту - {} не установлено! - {}",
-                    port, e
-                );
-                continue;
-            }
-        };
-
-        let request = b"USER anonymous\r\n";
-        match stream.write_all(request).await {
-            Ok(_) => (),
-            Err(e) => warn!("Ошибка отправки запроса! - {}", e),
-        }
-
-        let mut buffer = [0; 1024];
-        match stream.read(&mut buffer).await {
-            Ok(read) => {
-                let response = &buffer[0..read];
-                let buffered_text = String::from_utf8_lossy(response);
-                let target_line = "No anonymous login";
-
-                if buffered_text.contains(target_line) {
-                    anon_auths.push(Features::FTPAuth(format!(
-                        "FTP connect => Rejected - {}:{}",
-                        target, port
-                    )))
-                } else {
-                    anon_auths.push(Features::FTPAuth(format!(
-                        "FTP connect => Accepted - {}:{}",
-                        target, port
-                    )))
-                }
-            }
-            Err(e) => warn!("Ошибка чтения! - {}", e),
         }
     }
-    Ok(anon_auths)
+    Ok(features)
 }
+
+fn get_status_auth(buffered_text: &str, target: &IpAddr, port: &u16) -> Features {
+    let target_line = "No anonymous login";
+
+    return if buffered_text.contains(target_line) {
+        FTPAuth(format!("Authorization Rejected -> {}:{}", target, port))
+    } else {
+        FTPAuth(format!("Authorization Accepted -> {}:{}", target, port))
+    };
+}
+
+async fn send_request(stream: &mut TcpStream) {
+    let request = b"USER anonymous\r\n";
+    ProtocolOperation::write_request(request, stream).await;
+}
+
+async fn get_response_request(stream: &mut TcpStream, buffer: &mut [u8; 1024]) -> Option<usize> {
+    if let Some(read) = ProtocolOperation::read_request(buffer, stream).await {
+        return Some(read);
+    }
+    None
+}
+
+async fn establish_connection(endpoint: &str, port: &u16) -> Option<TcpStream> {
+    ProtocolOperation::get_tcp_connection_stream(endpoint, port).await
+}
+
+fn convert_text_to_string(not_converted_text: &[u8], read: usize) -> String {
+    let text = ProtocolOperation::get_converted_response_to_utf8(not_converted_text, read);
+    text.to_string()
+}
+
